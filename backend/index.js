4940e26b029d056c75e815e95f66d9db938ba511
tanha478 <dcry049@gmail.com>
2025-03-31 11:51:50 +0530

    updating

--- backend/index.js
+++ backend/index.js
@@ -67,11 +67,11 @@ app.use((req, res, next) => {
 
 
 app.use((req, res, next) => {
-  const allowedOrigins = ["https://zerodha-clone-frontend-ndqw.onrender.com", "https://zerodha-clone-dashboard-fdei.onrender.com"];
-  const origin = req.headers.origin || "https://zerodha-clone-frontend-ndqw.onrender.com";
-  if (allowedOrigins.includes(origin)) {
+  // const allowedOrigins = ["https://zerodha-clone-frontend-ndqw.onrender.com", "https://zerodha-clone-dashboard-fdei.onrender.com"];
+  // const origin = req.headers.origin || "https://zerodha-clone-frontend-ndqw.onrender.com";
+  // if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", "https://zerodha-clone-frontend-ndqw.onrender.com");
-  }
+  // }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
