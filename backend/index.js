e4463621dc781635074ff3a1c3aaed0b8ffc02e0
tanha478 <dcry049@gmail.com>
2025-03-31 09:41:05 +0530

    updating

--- backend/index.js
+++ backend/index.js
@@ -26,12 +26,17 @@ app.use(cors({
 }));
 
 app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  const allowedOrigins = ["https://zerodha-clone-frontend-ndqw.onrender.com", "https://zerodha-clone-dashboard-fdei.onrender.com"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.send();
 });
+
 
 
 app.use(bodyParser.json());
