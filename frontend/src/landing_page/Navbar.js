88242c219013759b8dd6bd3c7e677da993ea66e2
tanha478 <dcry049@gmail.com>
2025-04-06 12:58:04 +0530

    updating

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -27,8 +27,8 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <form className="d-flex" role="search">
-            <ul className="navbar-nav mb-lg-0">
+          {/* <form className="d-flex" role="search"> */}
+            <ul className="navbar-nav mb-lg-0 d-lg-none">
               <li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/signup">
                   Signup
@@ -60,7 +60,7 @@ function Navbar() {
                 </Link>
               </li>
             </ul>
-          </form>
+          {/* </form> */}
         </div>
       </div>
     </nav>
